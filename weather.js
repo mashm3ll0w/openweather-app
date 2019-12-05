1a803af35551146bcea954e513958d9fb9c21fea
mashm3ll0w <charlswall@live.com>
2019-12-05 16:48:58 +0300

    i can fetch data from the API

--- weather.js
+++ weather.js
@@ -3,20 +3,31 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 const request = require("request");
 const port = process.env.PORT || 3515;
+const API_KEY = "";	// Enter your API Key Here
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/html/index.html");
+	res.sendFile(__dirname + "/public/html/index.html");
 });
 
 app.post("/", (req, res) => {
-  res.send("Post Works");
+	var baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+	var city = req.body.cityName;
+	var fullURL = baseURL + city + "&APPID=" + API_KEY;
+
+	request(fullURL, (error, response, body) => {
+		console.log("error:", error);
+		console.log("response: ", response.statusCode);
+		var data = JSON.parse(body);
+		res.send();
+	});
 });
 
 app.listen(port, (req, res) => {
-  console.log(`Listening on port ${port}...`);
+	console.log(`Listening on port ${port}...`);
 });
+
